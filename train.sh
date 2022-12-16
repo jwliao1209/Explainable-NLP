6cc54392a9f31ab0dd1249f752e7ebde418e80cc
jiawei <sam23582211@gmail.com>
2022-12-17 00:43:08 +0800

    update train.sh

--- train.sh
+++ train.sh
@@ -3,8 +3,8 @@ export CUDA_VISIBLE_DEVICES=0
 python run_summarization.py \
     --do_train \
     --model_name_or_path t5-small \
-    --train_file dataset/train_dataset_qr_q.json \
-    --output_dir checkpoint/t5_base_qr_q_bs4 \
+    --train_file dataset/train_dataset_all_q.json \
+    --output_dir checkpoint/t5_small_qr_q_bs4 \
     --per_device_train_batch_size=4 \
     --gradient_accumulation_steps=4 \
     --predict_with_generate \
